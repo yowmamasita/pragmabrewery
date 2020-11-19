864cdbf3eb0c9b2922df0276f5430ef970e3344d
Ben Sarmiento <ben.sarmiento@foodpanda.com>
2020-11-20 00:27:54 +0800

    Add newman tests

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { AppConfig } from '../config';
 import { Registry } from '../registry';
 
 export const runServer = (config: AppConfig): void => {
   const app = express();
+  app.use(cors());
   app.get('/', (req, res) => res.send('/beer-temperatures is where it\'s at!'));
   const registry = new Registry(config);
   registry.registerBeersHandler(app);
   app.listen(config.general.port, () => {
+    // eslint-disable-next-line no-console
     console.log(`The server is now running at port ${config.general.port}`, config);
   });
 };

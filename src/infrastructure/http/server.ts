b9e79a968eff4d106e797e57d7fb7a1b2ab9117c
Ben Sarmiento <ben.sarmiento@foodpanda.com>
2020-11-19 19:12:04 +0800

    Add default message on root route

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -4,7 +4,7 @@ import { Registry } from '../registry';
 
 export const runServer = (config: AppConfig): void => {
   const app = express();
-  app.get('/', (req, res) => res.send('hello'));
+  app.get('/', (req, res) => res.send('/beers is where it\'s at!'));
   const registry = new Registry(config);
   registry.registerBeersHandler(app);
   app.listen(config.general.port, () => {
